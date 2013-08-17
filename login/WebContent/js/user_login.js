9223eb9d42bfeaced3e009b1b9f541229ea52f88
Wubai Zhou <zhouwubai@gmail.com>
2013-08-17 11:56:57 -0400

    add redirecting if user not login.

--- login/WebContent/js/user_login.js
+++ login/WebContent/js/user_login.js
@@ -3,23 +3,22 @@
  * @param xPos apply right top-corner coordinate system to this logger div
  * @param yPos
  */
-function applyFiuUserLogger(xPos,yPos){
+function applyFiuUserLogger(xPos,yPos,main_page){
 	
 	var loginHtml = getLoginHtml();
 	var loginBtn = getLoginButton();
 	
 /**	if(indicator)
-		$(selector).before(loginBtn);
+		$('body').before(loginBtn);
 	else
 		$(selector).after(loginBtn);
-**/	
-	
-	$(loginBtn).appendTo('body');
+
+**/	$(loginBtn).appendTo('body');
 	$(loginHtml).appendTo('body');
 	$(".login_status").css("right",xPos);
 	$(".login_status").css("top",yPos);
 	
-	initialLoginStatus();
+	initialLoginStatus(main_page);
 	
 	$('#login-logout-tab').click(function(){
 		$.post('UserAliveServlet', function(data){
@@ -125,12 +124,16 @@ function logoutStatus(){
 
 
 
-function initialLoginStatus(){
+function initialLoginStatus(main_page){
 	$.post('UserAliveServlet', function(data){
 		if (data.result == 'true') {
 			logedInStatus(data.nick_name);
 		}
 		else {
+			if(window.location.href.indexOf(main_page) == -1){
+				alert("对不起，请选登录。");
+				window.location.href = main_page;
+			}
 			logoutStatus();
 		}
 	}, 'json');
@@ -142,5 +145,7 @@ function logedInStatus(nick_name){
 	$('.login_status #user_info').text(nick_name);
 	$('.login_status #login-logout-tab').text("注销");
 }
+
+
 
 
